7f8bc7199306dc03618a3983939ad4b35209d7ae
Nikita <zatokino@gmail.com>
2025-09-05 15:54:32 +0300

    restart CI

--- k6/users_test.js
+++ k6/users_test.js
@@ -1,5 +1,6 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
+import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
 
 export let options = {
   vus: 50,          // количество виртуальных пользователей
@@ -15,4 +16,11 @@ export default function () {
   });
 
   sleep(1);
+}
+
+// Генерация HTML отчета
+export function handleSummary(data) {
+  return {
+    'site/k6/index.html': htmlReport(data),
+  };
 }
