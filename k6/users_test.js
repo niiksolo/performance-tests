3f3a64d32957752dc6687afa46bf86c567c89062
Nikita <zatokino@gmail.com>
2025-09-19 22:18:09 +0300

    README fix

--- k6/users_test.js
+++ k6/users_test.js
@@ -3,17 +3,17 @@ import { sleep, check } from 'k6';
 import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
 
 export let options = {
-  vus: 10,           // количество виртуальных пользователей
-  duration: '30s',   // время нагрузки
+  vus: 10,
+  duration: '30s',
   thresholds: {
-    'http_req_duration': ['p(95)<500'], // 95% запросов < 500ms
+    'http_req_duration': ['p(95)<500'],
   },
 };
 
 export default function () {
   let res = http.get('https://reqres.in/api/users?page=2');
 
-  // Функциональные проверки
+
   check(res, {
     'status is 200': (r) => r.status === 200,
     'page = 2': (r) => r.json().page === 2,
@@ -21,10 +21,10 @@ export default function () {
     'first user has id': (r) => r.json().data[0].id !== undefined,
   });
 
-  sleep(1); // имитация паузы между итерациями
+  sleep(1);
 }
 
-// Генерация HTML отчета
+
 export function handleSummary(data) {
   return {
     'site/k6/index.html': htmlReport(data),
